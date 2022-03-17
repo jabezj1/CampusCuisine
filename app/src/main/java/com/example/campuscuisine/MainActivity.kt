c9ebbd468d11babdd29b451e6682ce1c00be84c9
jabezj1 <jabezj1@gmail.com>
2022-03-17 10:03:18 -0400

    splashscreen commit

--- app/src/main/java/com/example/campuscuisine/MainActivity.kt
+++ app/src/main/java/com/example/campuscuisine/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.campuscuisine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
