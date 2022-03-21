24eed4aee4c7149231099877c0b3a6a4804dc88c
jabezj1 <jabezj1@gmail.com>
2022-03-21 15:39:20 -0400

    SignUp page Version 1.02

--- app/src/main/java/com/example/campuscuisine/SignupActivity.kt
+++ app/src/main/java/com/example/campuscuisine/SignupActivity.kt
@@ -1,12 +1,21 @@
 package com.example.campuscuisine
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        findViewById<ImageButton>(R.id.btnBack).setOnClickListener {
+            val intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
