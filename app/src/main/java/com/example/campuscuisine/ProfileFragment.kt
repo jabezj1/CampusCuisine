3a271773770761f1ac40bbe5eef73b39ff9e7ff7
jabezj1 <jabezj1@gmail.com>
2022-04-06 22:13:58 -0400

    new commit 3/31

--- app/src/main/java/com/example/campuscuisine/ProfileFragment.kt
+++ app/src/main/java/com/example/campuscuisine/ProfileFragment.kt
@@ -1,10 +1,12 @@
 package com.example.campuscuisine
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 
 
 class ProfileFragment : Fragment() {
@@ -14,5 +16,6 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+
     }
 }
