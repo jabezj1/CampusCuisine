e93fcc29ea03e89308307bf3e44493d3a5bcd225
Your Name <kevinulcena05@gmail.com>
2022-04-08 21:57:33 -0400

    Checkout Fragment Version 1.02

--- app/src/main/java/com/example/campuscuisine/CheckoutFragment.kt
+++ app/src/main/java/com/example/campuscuisine/CheckoutFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class CheckoutFragment : Fragment() {
@@ -14,5 +16,34 @@ class CheckoutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_checkout, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerview = view?.findViewById<RecyclerView>(R.id.recyclerview)
+
+        // this creates a vertical layout Manager
+        if (recyclerview != null) {
+            recyclerview.layoutManager = LinearLayoutManager(activity)
+        }
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<ItemsViewModel>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..20) {
+            data.add(ItemsViewModel("$"+i, "Item " + i))
+        }
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = CustomAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        if (recyclerview != null) {
+            recyclerview.adapter = adapter
+        }
     }
 }
